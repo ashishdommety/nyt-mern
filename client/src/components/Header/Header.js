71ce679439f7c761bff20c699046230403577036
unknown <ashishuiux@gmail.com>
2017-09-21 00:48:22 -0700

    add hyperlink buttons to change route

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
 
 class Header extends Component{
+  constructor(){
+    super();
+    this.state = {
+
+    }
+    this.gotoSaved = this.gotoSaved.bind(this);
+    this.gotoHome = this.gotoHome.bind(this);
+  }
+  gotoSaved = () => {
+    window.location.pathname = "/saved";
+  }
+  gotoHome = () => {
+    window.location.pathname = "/"
+  }
   render(){
     return(
       <div>
         <h1>New York Times Article Scrubber!</h1>
         <h3> Search and save articles you love </h3>
-        <button>Check Saved</button>
-        <button>Home</button>
+        <button onClick={this.gotoHome}>Home</button>
+        <button onClick={this.gotoSaved}>Check Saved</button>
       </div>
     );
   }
